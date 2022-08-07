83f32dc7783c59c22f4af4efeb82042a1029008c
KonstantinDenisov <omacs@mail.ru>
2022-08-08 01:25:08 +0300

    change pad-image

--- Assets/Scripts/Pad.cs
+++ Assets/Scripts/Pad.cs
@@ -31,7 +31,6 @@ public class Pad : MonoBehaviour
         {
             Vector3 mousePositionInPixels = Input.mousePosition;
             Vector3 mousePositionInUnits = Camera.main.ScreenToWorldPoint(mousePositionInPixels);
-
             Vector3 currentPosition = transform.position;
             currentPosition.x = mousePositionInUnits.x;
             transform.position = currentPosition; 
